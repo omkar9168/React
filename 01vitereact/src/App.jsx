a3c5feee27706cfe48cfd72b63feec563963276c
omkar2024 <omkarghadge2126@gmail.com>
2024-09-11 12:15:50 +0530

    first commit

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,19 @@
+
+
+import Chai from "./chai"
+
+
+function App() {
+  const username = "chai aur code"
+
+  return (
+    <>
+   <Chai/>
+   <h1>chai aur react {username}</h1>
+   <p>Test Pass</p> 
+
+   </>
+  )
+}
+
+export default App
